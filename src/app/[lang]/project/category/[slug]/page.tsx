01b339c205b622d3bbfb2f0f62249bfbf5442069
Jojo <azizditya@gmail.com>
2025-10-11 12:09:02 +0700

    add static params project

--- src/app/[lang]/project/category/[slug]/page.tsx
+++ src/app/[lang]/project/category/[slug]/page.tsx
@@ -1,4 +1,19 @@
 import ProjectPage__List from "../../list";
+import supabaseClient from "@/supabase/client";
+
+export async function generateStaticParams() {
+  const { data: categories } = await supabaseClient
+    .from("project_categories")
+    .select("slug")
+    .is("deleted_at", null)
+    .order("order");
+
+  return (
+    categories?.map((category) => ({
+      slug: category.slug,
+    })) ?? []
+  );
+}
 
 export default async function ProjectCategoryPage({
   params,
