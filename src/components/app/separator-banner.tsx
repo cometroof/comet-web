8c9fc68433f6632289399a806a9965701b5129e0
Jojo <azizditya@gmail.com>
2025-10-17 18:01:03 +0700

    update separator banner

--- src/components/app/separator-banner.tsx
+++ src/components/app/separator-banner.tsx
@@ -1,18 +1,34 @@
+import Image from "next/image";
 import { ReactNode } from "react";
 
 export default function SeparatorBanner({
   imgUrl,
   children,
+  optimized,
+  height,
 }: {
   imgUrl?: string;
   children?: ReactNode;
+  optimized?: boolean;
+  height?: number;
 }) {
+  let hSection = "h-[600px]";
+  if (height) hSection = `h-[${height}px]`;
   return (
-    <div className="h-[600px] sticky top-header w-full">
+    <div className={`${hSection} sticky top-header w-full`}>
       <div className="size-full relative">
-        {imgUrl && (
-          <img src={imgUrl} alt="banner" className="size-full object-cover" />
-        )}
+        {imgUrl &&
+          (optimized ? (
+            <Image
+              width={100}
+              height={height || 600}
+              src={imgUrl}
+              alt=""
+              className="size-full object-cover"
+            />
+          ) : (
+            <img src={imgUrl} alt="banner" className="size-full object-cover" />
+          ))}
         {children}
       </div>
     </div>
