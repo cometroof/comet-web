5b33e6d35d23720f2fc12058bcf93c5b4af268be
Jojo <azizditya@gmail.com>
2025-10-30 13:02:14 +0700

    Add product renderer

--- src/app/[lang]/product/[slug]/page.tsx
+++ src/app/[lang]/product/[slug]/page.tsx
@@ -8,16 +8,27 @@ interface Props extends ParamsLang {
 }
 
 async function getProductData(slug: string) {
-  return (
-    (
-      await supabaseClient
-        .from("product")
-        .select("*,product_category(*),product_profile(*),product_item(*)")
-        .eq("slug", slug)
-        .is("is_under_product", true)
-        .single()
-    ).data || null
-  );
+  const res = await supabaseClient
+    .from("product")
+    .select(
+      `*,product_category(name,subtitle),product_profile(*,product_category(id,name,subtitle),product_profile_badges(*,product_badges(id,name,image)),product_profile_certificates(*,certificates(*))),product_item(*)`,
+    )
+    .eq("slug", slug)
+    .is("is_under_product", true)
+    .single();
+  const managed = res.data;
+  if (managed?.product_profile) {
+    managed.product_profile.forEach((profile) => {
+      if (profile.product_profile_certificates) {
+        profile.product_profile_certificates.sort((a, b) => {
+          const orderA = a.certificates?.order ?? Number.MAX_SAFE_INTEGER;
+          const orderB = b.certificates?.order ?? Number.MAX_SAFE_INTEGER;
+          return orderA - orderB;
+        });
+      }
+    });
+  }
+  return managed || null;
 }
 
 export async function generateStaticParams() {
--- src/app/[lang]/product/[slug]/product-detail-page.tsx
+++ src/app/[lang]/product/[slug]/product-detail-page.tsx
@@ -2,34 +2,72 @@ import { Database } from "@/supabase/supabase";
 import { ParamsLang } from "../../types-general";
 import Link from "next/link";
 import BrandButton from "@/components/app/brand-button";
-import { Download } from "lucide-react";
+import { Check, Download } from "lucide-react";
 import FooterNew from "@/components/app/footer";
 import { getPageDictionary } from "../../dictionaries";
 import { ProductDetailDictionary } from "@/types/dictionary";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 
 type Product = Database["public"]["Tables"]["product"]["Row"];
+type Certificate = Database["public"]["Tables"]["certificates"]["Row"];
 type ProductProfile = Database["public"]["Tables"]["product_profile"]["Row"];
 type ProductCategory = Database["public"]["Tables"]["product_category"]["Row"];
 type ProductItem = Database["public"]["Tables"]["product_item"]["Row"];
+type ProductBadge = Database["public"]["Tables"]["product_badges"]["Row"];
+type ProductCertificate =
+  Database["public"]["Tables"]["product_certificates"]["Row"];
+type ProfileBadge =
+  Database["public"]["Tables"]["product_profile_badges"]["Row"];
+type ProfileCertificate =
+  Database["public"]["Tables"]["product_profile_certificates"]["Row"];
 
-export type ProductData = Product & {
-  product_profile: (ProductProfile & {
-    product_category?: ProductCategory[];
-  })[];
+type ProfileCertificatesRelation = ProfileCertificate & {
+  certificates?: Certificate;
 };
 
-export type ProductDataWithItems = Product & {
+type ProfileBadgeRelation = ProfileBadge & {
+  product_badges?: ProductBadge;
+};
+
+type ProductProfileRelations = ProductProfile & {
+  product_profile_badges?: ProfileBadgeRelation[];
+  product_profile_certificates?: ProfileCertificatesRelation[];
   product_category?: ProductCategory[];
-  product_profile?: ProductProfile[];
-  product_item?: ProductItem[];
 };
 
-interface ProductFetch extends Product {
-  product_profile: Partial<ProductProfile>[];
-}
+type ProductDataWithItems = Product & {
+  product_category?: ProductCategory[];
+  product_profile?: ProductProfileRelations[];
+  product_item?: ProductItem[];
+  product_badges?: ProductBadge[];
+  product_certificates?: ProductCertificate[];
+};
 
 interface Props extends ParamsLang {
   data: ProductDataWithItems;
+}
+
+function ProductItem(item: ProductItem) {
+  return (
+    <div className="relative" aria-label={`Product ${item.name}`}>
+      <div className="aspect-square relative bg-app-light-gray">
+        {item.image && (
+          <img
+            alt={item.name}
+            src={item.image}
+            className="block size-full object-cover"
+          />
+        )}
+      </div>
+      <div className="text-subheading break-words">{item.name}</div>
+    </div>
+  );
 }
 
 export default async function ProductDetailPage({ lang, data }: Props) {
@@ -40,6 +78,247 @@ export default async function ProductDetailPage({ lang, data }: Props) {
   let desc = data.description_en;
   if (lang === "id" && data.description_id) desc = data.description_id;
   const suitables = data.suitables as string[];
+  const profiles = data.product_profile as ProductProfileRelations[];
+
+  const productRenderer = ({ products }: { products: ProductItem[] }) => {
+    return (
+      <div className="mt-5 grid grid-cols-2 lg:grid-cols-4 gap-3 lg:gap-7">
+        {products.map((item) => (
+          <ProductItem key={item.id} {...item} />
+        ))}
+      </div>
+    );
+  };
+
+  const categorizedView = ({
+    category,
+    products,
+  }: {
+    category: ProductCategory;
+    products?: ProductItem[];
+  }) => {
+    return (
+      <section
+        key={`Category ${category.id} ${category.name}`}
+        className="outer-wrapper relative"
+      >
+        <div className="inner-wrapper border-t border-t-app-gray pt-5">
+          <div className="flex items-center gap-3">
+            <h3 className="text-heading2">{category.name}</h3>
+            {category.subtitle && (
+              <>
+                <div className="text-heading2">|</div>
+                <div className="text-caption">{category.subtitle}</div>
+              </>
+            )}
+          </div>
+          {products &&
+            productRenderer({
+              products,
+            })}
+        </div>
+      </section>
+    );
+  };
+
+  const profileListView = (
+    <>
+      <section className="outer-wrapper bg-app-light-gray py-[100px] relative">
+        <div className="inner-wrapper">
+          <div className="w-full overflow-x-auto hide-scrollbar">
+            <div className="flex min-w-full w-fit justify-center gap-10 lg:gap-20">
+              {profiles.map((p) => (
+                <div key={p.id} className="w-[240px] text-center">
+                  {p.profile_image_url && (
+                    <img
+                      src={p.profile_image_url}
+                      alt={p.name}
+                      className="block"
+                    />
+                  )}
+                  <h3 className="text-heading2 pt-5">{p.name}</h3>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
+      </section>
+      {profiles.map((p) => {
+        const sizes = p.size as {
+          name: string;
+          weight: string;
+          thickness: string;
+        }[];
+        return (
+          <section key={p.id} className="outer-wrapper bg-app-white relative">
+            <div className="inner-wrapper">
+              <div className="flex items-start gap-10 justify-between">
+                {/*PRODUCTS*/}
+                <div className="w-2/3">
+                  {p.product_category &&
+                    p.product_category.map((c) =>
+                      categorizedView({
+                        category: c,
+                        products: data.product_item?.filter(
+                          (i) =>
+                            i.product_profile_id === p.id &&
+                            i.product_category_id === c.id,
+                        ),
+                      }),
+                    )}
+                </div>
+                {/*INFORMATION*/}
+                <div className="flex-1 space-y-2.5 [&>div]:not-last:border-b [&>div]:border-b-app-gray [&>div]:pb-5">
+                  {/*INFORMATION NAME*/}
+                  <div>
+                    <h2 className="text-heading1">{p.name}</h2>
+                  </div>
+                  {/*INFORMATION SIZE*/}
+                  {sizes.length > 0 && (
+                    <div>
+                      <Table>
+                        <TableHeader>
+                          <TableRow>
+                            <TableCell className="font-exo-2 text-sm font-bold">
+                              Available Size
+                            </TableCell>
+                            {sizes.map((s) => (
+                              <TableCell
+                                key={`${p.id}-${s.name}`}
+                                className="font-exo-2 text-sm font-bold"
+                              >
+                                {s.name}
+                              </TableCell>
+                            ))}
+                          </TableRow>
+                        </TableHeader>
+                        <TableBody>
+                          <TableRow>
+                            <TableCell className="font-exo-2 text-sm font-bold">
+                              Thickness (mm)
+                            </TableCell>
+                            {sizes.map((s) => (
+                              <TableCell key={`${p.id}-${s.name}`}>
+                                {s.thickness}
+                              </TableCell>
+                            ))}
+                          </TableRow>
+                          <TableRow>
+                            <TableCell className="font-exo-2 text-sm font-bold">
+                              Weight (g)
+                            </TableCell>
+                            {sizes.map((s) => (
+                              <TableCell key={`${p.id}-${s.name}`}>
+                                {s.weight}
+                              </TableCell>
+                            ))}
+                          </TableRow>
+                        </TableBody>
+                      </Table>
+                    </div>
+                  )}
+                  {/*INFORMATION DIMENSION*/}
+                  <div>
+                    <Table>
+                      <TableBody>
+                        {p.size_per_panel && (
+                          <TableRow className="border-b-transparent">
+                            <TableCell className="p-1 font-exo-2 text-sm font-bold w-[140px]">
+                              Size per panel
+                            </TableCell>
+                            <TableCell className="p-1">
+                              {p.size_per_panel}
+                            </TableCell>
+                          </TableRow>
+                        )}
+                        {p.effective_size && (
+                          <TableRow className="border-b-transparent">
+                            <TableCell className="p-1 font-exo-2 text-sm font-bold w-[140px]">
+                              Effective size
+                            </TableCell>
+                            <TableCell className="p-1">
+                              {p.effective_size}
+                            </TableCell>
+                          </TableRow>
+                        )}
+                        {p.panel_amount && (
+                          <TableRow className="border-b-transparent">
+                            <TableCell className="p-1 font-exo-2 text-sm font-bold w-[140px]">
+                              Panel Ammount/m
+                              <span className="align-super">2</span>
+                            </TableCell>
+                            <TableCell className="p-1">
+                              {p.panel_amount}
+                            </TableCell>
+                          </TableRow>
+                        )}
+                      </TableBody>
+                    </Table>
+                  </div>
+                  {/*INFORMATION CERTIFICATES*/}
+                  <div>
+                    <div className="font-exo-2 text-sm font-bold">
+                      Certifications
+                    </div>
+                    <div className="mt-3  grid grid-cols-2">
+                      {p?.product_profile_certificates?.map((c) => {
+                        let certName =
+                          c.certificates?.label_name || c.certificates?.name;
+                        if (
+                          lang === "id" &&
+                          (c.certificates?.label_name_id ||
+                            c.certificates?.name_id)
+                        )
+                          certName =
+                            c.certificates?.label_name_id ||
+                            c.certificates?.name_id ||
+                            c.certificates.label_name ||
+                            c.certificates.name;
+                        return (
+                          <div
+                            key={c.id}
+                            className="text-caption flex items-start gap-2"
+                          >
+                            <div className="size-3 flex items-center justify-center rounded-full bg-primary text-background mt-1">
+                              <Check className="size-2" />
+                            </div>
+                            <div className="flex-1 break-words">{certName}</div>
+                          </div>
+                        );
+                      })}
+                    </div>
+                    {(p.product_profile_badges?.length || 0) > 0 && (
+                      <div className="mt-6 flex gap-5 ">
+                        {p.product_profile_badges?.map((b) => (
+                          <div className="size-[66px] relative" key={b.id}>
+                            <img
+                              alt={b.product_badges?.name}
+                              src={b.product_badges?.image}
+                              className="size-full object-contain"
+                            />
+                          </div>
+                        ))}
+                      </div>
+                    )}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+        );
+      })}
+    </>
+  );
+
+  const productListView = (
+    <section className="outer-wrapper relative">
+      <div className="inner-wrapper">
+        <div>{data.product_item?.length} Item</div>
+        {data.product_item && productRenderer({ products: data.product_item })}
+      </div>
+    </section>
+  );
+
   return (
     <>
       <h1 className="hidden">Cometroof - {data.name}</h1>
@@ -84,6 +363,7 @@ export default async function ProductDetailPage({ lang, data }: Props) {
                 {data.catalogue && (
                   <Link
                     href={data.catalogue}
+                    target="_blank"
                     aria-label={`Visit catalogue ${data.name} product`}
                   >
                     <BrandButton className="flex items-center">
@@ -100,9 +380,9 @@ export default async function ProductDetailPage({ lang, data }: Props) {
             <div className="border-t border-t-app-gray pt-5 pb-16 mt-16">
               <div className="text-caption">{copy.suitables_title}:</div>
               <div className="mt-8 overflow-x-auto hide-scrollbar">
-                <div className="w-fit flex gap-8 md:gap-14">
+                <div className="grid grid-cols-2 lg:grid-cols-4 gap-8 md:gap-14">
                   {suitables.map((s, n) => (
-                    <div key={n} className="w-[250px]">
+                    <div key={n} className="">
                       <div className="text-primary text-subheading">
                         {String(n + 1).padStart(2, "0")}
                       </div>
@@ -115,7 +395,19 @@ export default async function ProductDetailPage({ lang, data }: Props) {
           )}
         </div>
       </section>
-
+      {/*PROFILE THUMBS*/}
+      {profiles && profiles.length > 0
+        ? profileListView
+        : data.product_category && (data.product_category?.length || 0) > 0
+          ? data.product_category.map((c) =>
+              categorizedView({
+                category: c,
+                products: data.product_item?.filter(
+                  (i) => i.product_category_id === c.id,
+                ),
+              }),
+            )
+          : productListView}
       <FooterNew className="bg-app-white" />
     </>
   );
