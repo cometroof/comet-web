fe762486af089016e3191b4524fa159e4fe36633
Jojo <azizditya@gmail.com>
2025-11-19 23:44:36 +0700

    Adjust project fetching

--- src/app/[lang]/project/(list)/list.tsx
+++ src/app/[lang]/project/(list)/list.tsx
@@ -2,27 +2,28 @@ import { LangLink } from "@/components/app/lang-link";
 import supabaseClient from "@/supabase/client";
 import Image from "next/image";
 
-const getData = async (categorySlug: string) => {
-  const { data, error } = await supabaseClient
-    .from("project_categories")
-    .select(
-      `
-      id,
-      project_category_relations!inner(
-        projects!inner(
-          *,
-          project_images(*)
-        )
+const getData = async (categorySlug?: string) => {
+  if (categorySlug) {
+    const { data, error } = await supabaseClient
+      .from("project_categories")
+      .select(
+        `id,project_category_relations!inner(projects!inner(*,project_images(*)))`
       )
-    `
-    )
-    .eq("slug", categorySlug)
-    .is("deleted_at", null)
-    .single();
+      .eq("slug", categorySlug)
+      .is("deleted_at", null)
+      .single();
 
-  if (error || !data) return null;
+    if (error || !data) return null;
 
-  return data.project_category_relations.map((rel) => rel.projects);
+    return data.project_category_relations.map((rel) => rel.projects);
+  }
+
+  const { data: projects } = await supabaseClient
+    .from("projects")
+    .select("*, project_images(*)")
+    .order("order", { ascending: true });
+
+  return projects;
 };
 
 export default async function ProjectPage__List({
