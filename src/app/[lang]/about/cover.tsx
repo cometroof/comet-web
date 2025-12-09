24ffd1204559f3dceeae6915773fd904ed1f950b
Jojo <azizditya@gmail.com>
2025-12-09 14:14:36 +0700

    Adjust Company Profile link and Image renderer

--- src/app/[lang]/about/cover.tsx
+++ src/app/[lang]/about/cover.tsx
@@ -4,13 +4,27 @@ import { ParamsLang } from "../types-general";
 import { getPageDictionary } from "../dictionaries";
 import { AboutUsDictionary } from "@/types/dictionary";
 import { cleanHTML } from "../utils/utils";
+import supabaseClient from "@/supabase/client";
+import Link from "next/link";
+
+export async function getCompanyProfile() {
+  return (
+    await supabaseClient
+      .from("company_profile")
+      .select()
+      .eq("type", "company_profile")
+      .order("uploaded_at", { ascending: true })
+      .single()
+  ).data;
+}
 
 export default async function AboutUsPage__Cover({ lang }: ParamsLang) {
   const _lang = lang || "en";
   const { cover } = (await getPageDictionary(
     _lang,
-    "about"
+    "about",
   )) as AboutUsDictionary;
+  const companyProfile = await getCompanyProfile();
   return (
     <section className="outer-wrapper-x bg-app-light-gray py-[64px] lg:py-[106px]">
       <div className="inner-wrapper  flex flex-col lg:flex-row gap-10 lg:gap-20 lg:justify-between items-start">
@@ -26,10 +40,14 @@ export default async function AboutUsPage__Cover({ lang }: ParamsLang) {
             className="text-body lg:mt-10"
             dangerouslySetInnerHTML={{ __html: cleanHTML(cover.letter) }}
           ></p>
-          <BrandButton className="mt-10 lg:mt-16 btn-fill">
-            <Download className="size-5" />
-            <span>{cover.cta}</span>
-          </BrandButton>
+          {companyProfile?.file_url && (
+            <Link href={companyProfile.file_url} target="_blank">
+              <BrandButton className="mt-10 lg:mt-16 btn-fill">
+                <Download className="size-5" />
+                <span>{cover.cta}</span>
+              </BrandButton>
+            </Link>
+          )}
         </div>
       </div>
     </section>
--- src/app/[lang]/home/cover-things.tsx
+++ src/app/[lang]/home/cover-things.tsx
@@ -38,11 +38,9 @@ const HeroImage = ({ src, alt, isActive }: IHeroImage) => (
     src={src}
     className={cn(
       "absolute left-0 top-0 size-full object-cover transition-all",
-      isActive ? "opacity-100 duration-700" : "opacity-0 duration-700"
+      isActive ? "opacity-100 duration-700" : "opacity-0 duration-700",
     )}
-    width={100}
-    height={100}
-    unoptimized
+    fill
   />
 );
 
@@ -62,7 +60,7 @@ const HeroContent = ({ item, isActive, lang }: IHeroContent) => {
         "col-start-1 row-start-1 transition-all",
         isActive
           ? "opacity-100 z-10 duration-500 delay-75 translate-y-0"
-          : "opacity-0 z-0 pointer-events-none duration-300 translate-y-10"
+          : "opacity-0 z-0 pointer-events-none duration-300 translate-y-10",
       )}
     >
       <div
@@ -150,7 +148,7 @@ export default function Homepage__CoverThings({ lang, coverData }: Props) {
                   alt={item.title[_lang]!}
                   isActive={index === step}
                 />
-              )
+              ),
           )}
         </div>
       </div>
